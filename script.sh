91276d591dbe33839851ada0037f9ebb64ab4805
Andres Gaviria T <44479766+agavariat@users.noreply.github.com>
2019-12-05 10:46:53 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,27 @@
+#este script esta hecho para instalar odoo 12 en ubuntu18 server
+apt-get update && apt-get upgrade
+apt-get install postgresql -y
+wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
+echo "deb http://nightly.odoo.com/12.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
+apt-get update && apt-get install odoo
+sudo wget https://builds.wkhtmltopdf.org/0.12.1.3/wkhtmltox_0.12.1.3-1~bionic_amd64.deb
+sudo dpkg -i wkhtmltox_0.12.1.3-1~bionic_amd64.deb
+sudo apt-get install -f
+sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
+sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
+chmod 777 /usr/lib/python3/dist-packages/odoo/addons
+apt install unzip
+apt install python3-pip
+pip3 pip install pysftp
+cd /usr/lib/python3/dist-packages/odoo/addons
+git clone https://github.com/agavariat/ba_web_gantt_project.git
+git clone https://github.com/agavariat/ba_web_gantt.git
+git clone https://github.com/agavariat/muk_autovacuum.git
+git clone https://github.com/agavariat/muk_utils.git
+git clone https://github.com/agavariat/muk_web_theme.git
+git clone https://github.com/agavariat/om_account_accountant.git
+git clone https://github.com/agavariat/accounting_pdf_reports.git
+git clone https://github.com/agavariat/om_account_asset.git
+git clone https://github.com/agavariat/om_account_budget.git
+git clone https://github.com/Yenthe666/auto_backup.git
+reboot
