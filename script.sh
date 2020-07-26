64cdd5e36f8e7760c5a6aa6f2165cc9c04acc324
Andres Gaviria T <44479766+agavariat@users.noreply.github.com>
2020-07-26 11:25:17 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #este script esta hecho para instalar odoo 12 en ubuntu18 server
 #!/bin/bash
-dom="construvivienda.intresco.co"
-oIP="3.136.37.82"
+dom="gagttechnorium.co"
+oIP="18.225.6.101"
 apt-get update && apt-get upgrade -y
 apt-get install postgresql -y
 wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
