4dfc271d9317957c9ebe403930831adc08987904
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2021-08-27 16:22:54 -0500

    Create weppa.sh

---
+++ weppa.sh
@@ -0,0 +1,66 @@
+#este script esta hecho para instalar odoo 12 en ubuntu18 server
+#!/bin/bash
+chmod 777 -R /usr/lib/python3/dist-packages/odoo/addons
+apt install unzip
+apt install python3-pip -y
+pip3 install pysftp
+pip3 install xlrd
+pip3 install xlwt
+pip3 install boto3
+pip3 install python-barcode
+pip3 install pyopenssl
+pip3 install requests
+pip3 install xmltodict
+pip3 install PyQRCode
+pip3 install pypng
+pip3 install email-validator
+pip3 install num2words
+apt-get update
+cd
+cd /usr/lib/python3/dist-packages/odoo/addons
+git clone https://github.com/agavariat/l10n_co_res_partner.git
+git clone https://github.com/agavariat/l10n_co_pos_res_partner.git
+git clone https://github.com/agavariat/l10n_co_tax_extension.git
+git clone https://github.com/agavariat/l10n_co_e-invoice.git
+git clone https://github.com/agavariat/ba_web_gantt_project.git
+git clone https://github.com/agavariat/ba_web_gantt.git
+git clone https://github.com/agavariat/ba_task_checklist.git
+git clone https://github.com/agavariat/om_account_accountant.git
+git clone https://github.com/agavariat/accounting_pdf_reports.git
+git clone https://github.com/agavariat/om_account_asset.git
+git clone https://github.com/agavariat/om_account_budget.git
+git clone https://github.com/Yenthe666/auto_backup.git
+git clone https://github.com/agavariat/cenit_base.git
+git clone https://github.com/agavariat/contract.git
+git clone https://github.com/agavariat/web_favicon.git
+git clone https://github.com/agavariat/pos_stock_realtime.git
+git clone https://github.com/agavariat/odoo_pos_keyboard.git
+git clone https://github.com/agavariat/purchase_discount.git
+git clone https://github.com/agavariat/pos_order_mgmt.git
+git clone https://github.com/agavariat/muk_web_utils.git
+git clone https://github.com/agavariat/muk_web_theme.git
+git clone https://github.com/agavariat/muk_utils.git
+git clone https://github.com/agavariat/muk_autovacuum.git
+git clone https://github.com/agavariat/pos_personalized_sequence.git
+git clone https://github.com/agavariat/bi_pos_reports.git
+git clone https://github.com/agavariat/pos_kitchen_receipt_app.git
+git clone https://github.com/agavariat/pos_ticket.git
+git clone https://github.com/agavariat/purchase_order_automation.git
+git clone https://github.com/agavariat/barcodes_generator_product.git
+git clone https://github.com/agavariat/barcodes_generator_abstract.git
+git clone https://github.com/agavariat/pos_margin.git
+git clone https://github.com/agavariat/purchase_order_variant_mgmt.git
+git clone https://github.com/agavariat/web_widget_x2many_2d_matrix.git
+git clone https://github.com/agavariat/bi_crm_task.git
+git clone https://github.com/agavariat/bi_odoo_process_costing_manufacturing.git
+git clone https://github.com/agavariat/theme_comercio.git
+git clone https://github.com/agavariat/comercio_web.git
+git clone https://github.com/agavariat/payu.git
+git clone https://github.com/agavariat/automatic_backup.git
+git clone https://github.com/agavariat/web_search_with_and.git
+git clone https://github.com/agavariat/partner_email_check.git
+git clone https://github.com/agavariat/sale_commission.git
+git clone https://github.com/agavariat/partner_enhancement.git
+git clone https://github.com/agavariat/pos_cash_in_out_odoo.git
+chmod 777 -R /usr/lib/python3/dist-packages/odoo/addons/comercio_web
+reboot
