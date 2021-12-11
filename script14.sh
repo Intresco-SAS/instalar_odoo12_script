4eead50014e12964d2c8d8c77fb712ac421f6acb
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2021-12-11 09:53:37 -0500

    actualizacion con librerias

--- script14.sh
+++ script14.sh
@@ -3,15 +3,17 @@
 apt-get update && apt-get upgrade -y
 apt-get install postgresql -y
 wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
-echo "deb http://nightly.odoo.com/12.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
+echo "deb http://nightly.odoo.com/14.0/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
 apt-get update && apt-get install odoo -y
-git clone https://github.com/agavariat/wkhtmltox_0.12.5-1.bionic_amd64.git
-mv wkhtmltox_0.12.5-1.bionic_amd64/wkhtmltox_0.12.5-1.bionic_amd64.deb ~/
-sudo dpkg -i wkhtmltox_0.12.5-1.bionic_amd64.deb
-sudo apt-get install -f -y
-sudo ln -s /usr/local/bin/wkhtmltoimage /usr/bin
-sudo ln -s /usr/local/bin/wkhtmltopdf /usr/bin
+apt-get install xfonts-75dpi
+wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
+cp /usr/local/bin/wkhtmltoimage /usr/bin/wkhtmltoimage
+cp /usr/local/bin/wkhtmltopdf /usr/bin/wkhtmltopdf
 chmod 777 -R /usr/lib/python3/dist-packages/odoo/addons
+apt-get install git python3 python3-pip build-essential wget python3-dev python3-venv python3-wheel libxslt-dev libzip-dev libldap2-dev libsasl2-dev python3-setuptools node-less libjpeg-dev gdebi -y
+apt-get install libpq-dev python-dev libxml2-dev libxslt1-dev libldap2-dev libsasl2-dev libffi-dev
+sudo -H pip3 install -r https://raw.githubusercontent.com/odoo/odoo/master/requirements.txt
 apt install unzip
 apt install python3-pip -y
 pip3 install pysftp
@@ -27,7 +29,4 @@ pip3 install pypng
 pip3 install email-validator
 pip3 install num2words
 apt-get update
-cd
-cd /usr/lib/python3/dist-packages/odoo/addons
-chmod 777 -R /usr/lib/python3/dist-packages/odoo/addons/comercio_web
-reboot
+service odoo restart
