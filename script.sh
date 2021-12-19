446f3b07ed0d5d055c4d50acd62cc18c1be0f33a
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2021-12-19 18:36:52 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -29,6 +29,7 @@ pip3 install num2words
 apt-get update
 cd
 cd /usr/lib/python3/dist-packages/odoo/addons
+git config --global credential.helper store
 git clone https://github.com/agavariat/l10n_co_res_partner.git
 git clone https://github.com/agavariat/l10n_co_pos_res_partner.git
 git clone https://github.com/agavariat/l10n_co_tax_extension.git
