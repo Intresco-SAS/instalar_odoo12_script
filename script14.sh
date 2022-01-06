f1fae44a6394f5e882037e6dfcf877831a9821bf
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2022-01-05 21:58:52 -0500

    Update script14.sh

--- script14.sh
+++ script14.sh
@@ -31,9 +31,8 @@ pip3 install num2words
 apt-get update
 cd /usr/lib/python3/dist-packages/odoo/addons
 git config --global credential.helper store
-git clone https://github.com/agavariat/account_dynamic_reports.git
 git clone https://github.com/agavariat/auth_api_key.git
-git clone https://github.com/agavariat/auto_backup.git
+git clone -b 14.0 https://github.com/agavariat/auto_backup.git
 git clone https://github.com/agavariat/auto_odoo_db_and_file_backup.git
 git clone https://github.com/agavariat/base_vat_ext.git
 git clone https://github.com/agavariat/bi_hr_overtime_request.git
@@ -43,17 +42,16 @@ git clone -b 14.0 https://github.com/agavariat/bi_pos_reports.git
 git clone https://github.com/agavariat/bi_pos_reprint_reorder.git
 git clone https://github.com/agavariat/bi_pos_stock.git
 git clone https://github.com/agavariat/bi_pos_warehouse_management.git
-git clone https://github.com/agavariat/dev_payslip_cancel.git
 git clone https://github.com/Consultoria-y-desarrollo-Quemari/e-invoice-co.git
 git clone https://github.com/agavariat/flexibite_com_advance.git
 git clone https://github.com/agavariat/hw_escpos_network_printer.git
 git clone https://github.com/agavariat/hw_restaurant_ip_printer.git
 git clone https://github.com/agavariat/init_web_many2one_disable.git
 git clone https://github.com/agavariat/l10n_co_payroll.git
-git clone https://github.com/agavariat/l10n_co_pos_res_partner.git
+git clone -b 14.0 https://github.com/agavariat/l10n_co_pos_res_partner.git
 git clone -b 14.0 https://github.com/agavariat/l10n_co_res_partner.git
 git clone https://github.com/agavariat/mai_pos_custom_order_number.git
-git clone https://github.com/agavariat/muk_web_theme.git
+git clone -b 14.0 https://github.com/agavariat/muk_web_theme.git
 git clone -b 14.0 https://github.com/agavariat/nomina_colombia.git
 git clone https://github.com/agavariat/ob_pos_advance_receipt_note.git
 git clone https://github.com/agavariat/odoo_sale_rental.git
@@ -61,18 +59,17 @@ git clone https://github.com/agavariat/om_account_daily_reports.git
 git clone https://github.com/agavariat/payslip_cancel.git
 git clone https://github.com/agavariat/pos_analytic_account.git
 git clone https://github.com/agavariat/pos_kitchen_receipt_app.git
-git clone https://github.com/agavariat/pos_logo_change.git
+#git clone https://github.com/agavariat/pos_logo_change.git
 git clone https://github.com/agavariat/pos_orders_list.git
 git clone https://github.com/agavariat/pos_payment_change.git
 git clone -b 14.0 https://github.com/agavariat/pos_personalized_sequence.git
 git clone https://github.com/agavariat/pos_show_config_name.git
 git clone https://github.com/agavariat/pos_tax_display.git
-git clone https://github.com/agavariat/pos_tip_fixed_percent.git
-git clone https://github.com/agavariat/product_cost_with_taxes.git
+#git clone https://github.com/agavariat/product_cost_with_taxes.git
 git clone https://github.com/agavariat/product_warehouse_quantity.git
 git clone -b 14.0 https://github.com/agavariat/purchase_last_price_info.git
 git clone https://github.com/agavariat/pw_customer_required.git
-git clone https://github.com/agavariat/sale_order_automation.git
+#git clone https://github.com/agavariat/sale_order_automation.git
 git clone https://github.com/agavariat/sale_order_line_product_image.git
 git clone https://github.com/agavariat/sh_pos_product_code.git
 git clone https://github.com/agavariat/support_documents.git
