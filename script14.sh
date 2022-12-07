3f842bc6cb2c774086ce5dc2eea9e53c55a1ef3d
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2022-12-07 10:40:50 -0500

    Jerarquía en Cuentas Contables

--- script14.sh
+++ script14.sh
@@ -114,5 +114,8 @@ git clone https://github.com/agavariat/account_invoice_fiscal_position_update.gi
 git clone https://github.com/agavariat/project_category.git
 git clone -b 14.0 https://github.com/agavariat/pos_cash_in_out_odoo.git
 git clone -b 14.0 https://github.com/agavariat/aspl_stock_inventory_report.git
+git clone https://github.com/agavariat/oi_org_chart_account_group.git
+git clone https://github.com/agavariat/oi_account.git
+git clone https://github.com/agavariat/oi_org_chart.git
 apt-get update
 service odoo restart
