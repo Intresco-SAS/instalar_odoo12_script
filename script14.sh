9fbd34ea9b34305fdd3038a03da2575921c395a8
Andres Gaviria <44479766+agavariat@users.noreply.github.com>
2023-06-08 21:22:32 -0500

    stock_move_location

--- script14.sh
+++ script14.sh
@@ -141,6 +141,7 @@ git clone -b 14.0 https://github.com/agavariat/pos_auto_session_closed_app.git
 git clone -b 14.0 https://github.com/agavariat/pos_user_restriction.git
 git clone https://github.com/agavariat/dbfilter_from_header.git
 git clone -b 14.0 https://github.com/Intresco-SAS/bi_pos_multi_shop.git
+git clone -b 14.0 https://github.com/Intresco-SAS/stock_move_location.git
 apt-get update
 timedatectl set-timezone America/Bogota
 service odoo restart
