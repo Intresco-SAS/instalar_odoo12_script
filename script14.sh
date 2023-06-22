3f1a8b784c3c8a8ad03b993f28ba444f193aaa0b
Andres Gaviria <44479766+agavariat@users.noreply.github.com>
2023-06-22 10:54:59 -0500

    cambios de repo

--- script14.sh
+++ script14.sh
@@ -42,104 +42,103 @@ chmod 777 -R dian
 chmod 777 -R backups
 cd /usr/lib/python3/dist-packages/odoo/addons
 git config --global credential.helper store
-git clone -b 14.0 https://github.com/agavariat/auto_backup.git
-git clone https://github.com/agavariat/base_vat_ext.git
-git clone https://github.com/agavariat/bi_import_pos_order.git
-git clone https://github.com/agavariat/bi_pos_pay_later.git
-git clone -b 14.0 https://github.com/agavariat/bi_pos_reports.git
-git clone https://github.com/agavariat/bi_pos_reprint_reorder.git
-git clone https://github.com/agavariat/bi_pos_stock.git
-git clone https://github.com/agavariat/bi_pos_warehouse_management.git
-git clone -b 14.1 https://github.com/agavariat/e-invoice-co.git
-git clone https://github.com/agavariat/flexibite_com_advance.git
-git clone https://github.com/agavariat/hw_escpos_network_printer.git
-git clone https://github.com/agavariat/hw_restaurant_ip_printer.git
-git clone https://github.com/agavariat/init_web_many2one_disable.git
-git clone https://github.com/agavariat/l10n_co_payroll.git
-git clone -b 14.0 https://github.com/agavariat/l10n_co_pos_res_partner.git
-git clone https://github.com/agavariat/mai_pos_custom_order_number.git
-git clone -b 14.0 https://github.com/agavariat/muk_web_theme.git
-git clone -b 14.1 https://github.com/agavariat/nomina_colombia.git
-git clone -b 14.1 https://github.com/agavariat/ob_pos_advance_receipt_note.git
-git clone https://github.com/agavariat/odoo_sale_rental.git
-git clone https://github.com/agavariat/om_account_daily_reports.git
-git clone https://github.com/agavariat/pos_analytic_account.git
-git clone -b 14.2 https://github.com/agavariat/pos_kitchen_receipt_app.git
-git clone https://github.com/agavariat/bi_pos_return_order.git
-git clone https://github.com/agavariat/pos_orders_list.git
-git clone https://github.com/agavariat/pos_payment_change.git
-git clone -b 14.0 https://github.com/agavariat/pos_personalized_sequence.git
-git clone https://github.com/agavariat/pos_show_config_name.git
-git clone https://github.com/agavariat/pos_tax_display.git
-#git clone https://github.com/agavariat/product_cost_with_taxes.git
-git clone https://github.com/agavariat/product_warehouse_quantity.git
-git clone -b 14.0 https://github.com/agavariat/purchase_last_price_info.git
-git clone https://github.com/agavariat/pw_customer_required.git
-#git clone https://github.com/agavariat/sale_order_automation.git
-git clone https://github.com/agavariat/sale_order_line_product_image.git
-git clone https://github.com/agavariat/sh_pos_product_code.git
-git clone https://github.com/agavariat/pos_tip_fixed_percent.git
-git clone -b 14.0 https://github.com/agavariat/account_dynamic_reports.git
-git clone https://github.com/agavariat/base_account_budget.git
-git clone https://github.com/agavariat/bi_pos_stock.git
-git clone -b final https://github.com/agavariat/journal_reports.git
-git clone https://github.com/agavariat/pos_default_partner.git
-git clone -b 14.0 https://github.com/agavariat/purchase_discount.git
-git clone https://github.com/agavariat/sh_pos_exact_search.git
-git clone https://github.com/agavariat/invoice_analytic_account.git
-git clone https://github.com/agavariat/purchase_analytic_account.git
-git clone -b 14.0 https://github.com/agavariat/partner_email_check.git
-git clone https://github.com/agavariat/website_floating_whatsapp_icon.git
-git clone https://github.com/agavariat/bi_automated_sale_order.git
-git clone https://github.com/agavariat/product_cost_with_taxes.git
-git clone https://github.com/agavariat/cotizacion_pos_report.git
-git clone https://github.com/agavariat/website_axis_helpdesk.git
-git clone https://github.com/agavariat/analytic_account_enh.git
-git clone https://github.com/agavariat/mrp_analytic.git
-git clone https://github.com/agavariat/mrp_analytic_sale_project.git
-git clone https://github.com/agavariat/account_analytic_parent.git
-git clone https://github.com/agavariat/pass_analytic_acc_in_journal_app.git
-git clone https://github.com/agavariat/stock_analytic.git
-git clone -b 14.0 https://github.com/agavariat/droggol_theme_common.git
-git clone -b 14.0 https://github.com/agavariat/theme_prime.git
-git clone -b 14.0 https://github.com/agavariat/contract.git
-git clone -b 14.0 https://github.com/agavariat/payment_report_co.git
-git clone https://github.com/agavariat/account_menu.git
-git clone https://github.com/agavariat/account_reconciliation_widget.git
-git clone https://github.com/agavariat/od_journal_sequence.git
-git clone https://github.com/agavariat/purchase_order_type.git
-git clone https://github.com/agavariat/sale_order_type.git
-git clone -b 14.0 https://github.com/agavariat/is_customer_is_vendor.git
-git clone -b 14.0 https://github.com/agavariat/project_parent_task_filter.git
-git clone -b 14.0 https://github.com/agavariat/om_account_asset.git
-git clone https://github.com/agavariat/account_invoice_fiscal_position_update.git
-git clone https://github.com/agavariat/project_category.git
-git clone -b 14.0 https://github.com/agavariat/pos_cash_in_out_odoo.git
-git clone -b 14.0 https://github.com/agavariat/aspl_stock_inventory_report.git
-git clone https://github.com/agavariat/oi_org_chart_account_group.git
-git clone https://github.com/agavariat/oi_account.git
-git clone https://github.com/agavariat/oi_org_chart.git
-git clone https://github.com/agavariat/bi_material_purchase_requisitions.git
-git clone https://github.com/agavariat/payment_multi_write_off.git
-git clone https://github.com/agavariat/purchase_reception_status.git
-git clone https://github.com/agavariat/product_quantity_update_force_inventory.git
-git clone https://github.com/agavariat/stock_barcodes.git
-git clone -b 14.0 https://github.com/agavariat/pragtech_import_invoice.git
-git clone https://github.com/agavariat/account_invoice_refund_link.git
-git clone https://github.com/agavariat/project_timesheet_time_control.git
-git clone https://github.com/agavariat/hr_timesheet_task_stage.git
-git clone https://github.com/agavariat/web_ir_actions_act_multi.git
-git clone https://github.com/agavariat/account_move_name_sequence.git
-git clone https://github.com/agavariat/sh_timesheet_calendar.git
-git clone https://github.com/agavariat/account_netting.git
-git clone https://github.com/agavariat/web_ir_actions_act_view_reload.git
-git clone https://github.com/agavariat/hr_timesheet_task_domain.git
-git clone https://github.com/agavariat/account_lock_to_date.git
-git clone -b 14.0 https://github.com/agavariat/pos_order_to_sale_order.git
-git clone https://github.com/agavariat/database_backup_to_s3.git
-git clone -b 14.0 https://github.com/agavariat/pos_auto_session_closed_app.git
-git clone -b 14.0 https://github.com/agavariat/pos_user_restriction.git
-git clone https://github.com/agavariat/dbfilter_from_header.git
+git clone -b 14.0 https://github.com/Intresco-SAS/auto_backup.git
+git clone https://github.com/Intresco-SAS/base_vat_ext.git
+git clone https://github.com/Intresco-SAS/bi_import_pos_order.git
+git clone https://github.com/Intresco-SAS/bi_pos_pay_later.git
+git clone -b 14.0 https://github.com/Intresco-SAS/bi_pos_reports.git
+git clone https://github.com/Intresco-SAS/bi_pos_reprint_reorder.git
+git clone https://github.com/Intresco-SAS/bi_pos_stock.git
+git clone https://github.com/Intresco-SAS/bi_pos_warehouse_management.git
+git clone https://github.com/Intresco-SAS/e-invoice-co.git
+git clone https://github.com/Intresco-SAS/flexibite_com_advance.git
+git clone https://github.com/Intresco-SAS/hw_escpos_network_printer.git
+git clone https://github.com/Intresco-SAS/hw_restaurant_ip_printer.git
+git clone https://github.com/Intresco-SAS/init_web_many2one_disable.git
+git clone https://github.com/Intresco-SAS/l10n_co_payroll.git
+git clone -b 14.0 https://github.com/Intresco-SAS/l10n_co_pos_res_partner.git
+git clone https://github.com/Intresco-SAS/mai_pos_custom_order_number.git
+git clone -b 14.0 https://github.com/Intresco-SAS/muk_web_theme.git
+git clone -b 14.1 https://github.com/Intresco-SAS/nomina_colombia.git
+git clone -b 14.1 https://github.com/Intresco-SAS/ob_pos_advance_receipt_note.git
+git clone https://github.com/Intresco-SAS/odoo_sale_rental.git
+git clone https://github.com/Intresco-SAS/om_account_daily_reports.git
+git clone https://github.com/Intresco-SAS/pos_analytic_account.git
+git clone -b 14.0 https://github.com/Intresco-SAS/pos_kitchen_receipt_app.git
+git clone https://github.com/Intresco-SAS/bi_pos_return_order.git
+git clone https://github.com/Intresco-SAS/pos_orders_list.git
+git clone https://github.com/Intresco-SAS/pos_payment_change.git
+git clone -b 14.1-restrict https://github.com/Intresco-SAS/pos_personalized_sequence.git
+git clone https://github.com/Intresco-SAS/pos_show_config_name.git
+git clone https://github.com/Intresco-SAS/pos_tax_display.git
+#git clone https://github.com/Intresco-SAS/product_cost_with_taxes.git
+git clone https://github.com/Intresco-SAS/product_warehouse_quantity.git
+git clone -b 14.0 https://github.com/Intresco-SAS/purchase_last_price_info.git
+git clone https://github.com/Intresco-SAS/pw_customer_required.git
+#git clone https://github.com/Intresco-SAS/sale_order_automation.git
+git clone https://github.com/Intresco-SAS/sale_order_line_product_image.git
+git clone https://github.com/Intresco-SAS/sh_pos_product_code.git
+git clone https://github.com/Intresco-SAS/pos_tip_fixed_percent.git
+git clone -b 14.0 https://github.com/Intresco-SAS/account_dynamic_reports.git
+git clone https://github.com/Intresco-SAS/base_account_budget.git
+git clone https://github.com/Intresco-SAS/bi_pos_stock.git
+git clone -b final https://github.com/Intresco-SAS/journal_reports.git
+git clone https://github.com/Intresco-SAS/pos_default_partner.git
+git clone -b 14.0 https://github.com/Intresco-SAS/purchase_discount.git
+git clone https://github.com/Intresco-SAS/sh_pos_exact_search.git
+git clone https://github.com/Intresco-SAS/invoice_analytic_account.git
+git clone https://github.com/Intresco-SAS/purchase_analytic_account.git
+git clone -b 14.0 https://github.com/Intresco-SAS/partner_email_check.git
+git clone https://github.com/Intresco-SAS/website_floating_whatsapp_icon.git
+git clone https://github.com/Intresco-SAS/bi_automated_sale_order.git
+git clone https://github.com/Intresco-SAS/product_cost_with_taxes.git
+git clone https://github.com/Intresco-SAS/cotizacion_pos_report.git
+git clone https://github.com/Intresco-SAS/website_axis_helpdesk.git
+git clone https://github.com/Intresco-SAS/analytic_account_enh.git
+git clone https://github.com/Intresco-SAS/mrp_analytic.git
+git clone https://github.com/Intresco-SAS/mrp_analytic_sale_project.git
+git clone https://github.com/Intresco-SAS/account_analytic_parent.git
+git clone https://github.com/Intresco-SAS/pass_analytic_acc_in_journal_app.git
+git clone https://github.com/Intresco-SAS/stock_analytic.git
+git clone -b 14.0 https://github.com/Intresco-SAS/droggol_theme_common.git
+git clone -b 14.0 https://github.com/Intresco-SAS/theme_prime.git
+git clone -b 14.0 https://github.com/Intresco-SAS/contract.git
+git clone -b 14.0 https://github.com/Intresco-SAS/payment_report_co.git
+git clone https://github.com/Intresco-SAS/account_menu.git
+git clone https://github.com/Intresco-SAS/account_reconciliation_widget.git
+git clone https://github.com/Intresco-SAS/od_journal_sequence.git
+git clone https://github.com/Intresco-SAS/purchase_order_type.git
+git clone https://github.com/Intresco-SAS/sale_order_type.git
+git clone -b 14.0 https://github.com/Intresco-SAS/is_customer_is_vendor.git
+git clone -b 14.0 https://github.com/Intresco-SAS/project_parent_task_filter.git
+git clone -b 14.0 https://github.com/Intresco-SAS/om_account_asset.git
+git clone https://github.com/Intresco-SAS/account_invoice_fiscal_position_update.git
+git clone https://github.com/Intresco-SAS/project_category.git
+git clone -b 14.0 https://github.com/Intresco-SAS/pos_cash_in_out_odoo.git
+git clone -b 14.0 https://github.com/Intresco-SAS/aspl_stock_inventory_report.git
+git clone https://github.com/Intresco-SAS/oi_org_chart_account_group.git
+git clone https://github.com/Intresco-SAS/oi_account.git
+git clone https://github.com/Intresco-SAS/oi_org_chart.git
+git clone https://github.com/Intresco-SAS/bi_material_purchase_requisitions.git
+git clone https://github.com/Intresco-SAS/payment_multi_write_off.git
+git clone https://github.com/Intresco-SAS/purchase_reception_status.git
+git clone https://github.com/Intresco-SAS/product_quantity_update_force_inventory.git
+git clone https://github.com/Intresco-SAS/stock_barcodes.git
+git clone -b 14.0 https://github.com/Intresco-SAS/pragtech_import_invoice.git
+git clone https://github.com/Intresco-SAS/account_invoice_refund_link.git
+git clone https://github.com/Intresco-SAS/project_timesheet_time_control.git
+git clone https://github.com/Intresco-SAS/hr_timesheet_task_stage.git
+git clone https://github.com/Intresco-SAS/web_ir_actions_act_multi.git
+git clone https://github.com/Intresco-SAS/account_move_name_sequence.git
+git clone https://github.com/Intresco-SAS/sh_timesheet_calendar.git
+git clone https://github.com/Intresco-SAS/account_netting.git
+git clone https://github.com/Intresco-SAS/web_ir_actions_act_view_reload.git
+git clone https://github.com/Intresco-SAS/hr_timesheet_task_domain.git
+git clone https://github.com/Intresco-SAS/account_lock_to_date.git
+git clone https://github.com/Intresco-SAS/database_backup_to_s3.git
+git clone -b 14.0 https://github.com/Intresco-SAS/pos_auto_session_closed_app.git
+git clone -b 14.0 https://github.com/Intresco-SAS/pos_user_restriction.git
+git clone https://github.com/Intresco-SAS/dbfilter_from_header.git
 git clone -b 14.0 https://github.com/Intresco-SAS/bi_pos_multi_shop.git
 git clone -b 14.0 https://github.com/Intresco-SAS/stock_move_location.git
 git clone -b 14.0 https://github.com/Intresco-SAS/done_activity.git
