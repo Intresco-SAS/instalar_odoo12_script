a275990085e8534634eb6b6524409d6484b67206
Andres Gaviria <44479766+agavariat@users.noreply.github.com>
2023-07-28 14:16:32 -0500

    Update script14.sh

--- script14.sh
+++ script14.sh
@@ -173,6 +173,7 @@ git clone https://github.com/Intresco-SAS/telemetrik_custom.git
 git clone https://github.com/Intresco-SAS/project_template.git
 git clone https://github.com/Intresco-SAS/stock_inventory_analytic.git
 git clone https://github.com/Intresco-SAS/account_payment_advance_mac5.git
+git clone https://github.com/Intresco-SAS/sale_order_line_menu.git
 apt-get update
 timedatectl set-timezone America/Bogota
 service odoo restart
