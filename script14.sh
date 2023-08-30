ac9670a8654c3145bd068a43716eff9c02ef56f0
DanielGR <danielgiral93@gmail.com>
2023-08-30 15:19:40 -0500

    nuevos modulos

--- script14.sh
+++ script14.sh
@@ -174,6 +174,8 @@ git clone https://github.com/Intresco-SAS/project_template.git
 git clone https://github.com/Intresco-SAS/stock_inventory_analytic.git
 git clone https://github.com/Intresco-SAS/account_payment_advance_mac5.git
 git clone https://github.com/Intresco-SAS/sale_order_line_menu.git
+git clone https://github.com/Intresco-SAS/barcodes_generator_product.git
+git clone https://github.com/Intresco-SAS/barcodes_generator_abstract.git
 apt-get update
 timedatectl set-timezone America/Bogota
 service odoo restart
