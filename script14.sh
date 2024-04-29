4dc64543b7248fa0eda6984998dc6d32c4bcd424
Nelson Velez <103436625+NelsonV93@users.noreply.github.com>
2024-04-29 13:32:49 -0500

    bank-statement-import

--- script14.sh
+++ script14.sh
@@ -177,6 +177,7 @@ git clone https://github.com/Intresco-SAS/sale_order_line_menu.git
 git clone https://github.com/Intresco-SAS/barcodes_generator_product.git
 git clone https://github.com/Intresco-SAS/barcodes_generator_abstract.git
 git clone -b 14.0 https://github.com/Intresco-SAS/custom_dryrefor.git
+git clone -b 14.0 https://github.com/Intresco-SAS/bank-statement-import.git
 apt-get update
 timedatectl set-timezone America/Bogota
 service odoo restart
