00d093cfb6597cc4e622a3387443dc8c91d65683
DanielGR <danielgiral93@gmail.com>
2024-09-25 06:18:21 -0500

    nuevo modulo

--- script15.sh
+++ script15.sh
@@ -111,6 +111,7 @@ git clone -b 15.0 https://github.com/Intresco-SAS/to_refund_account.git
 git clone -b 15.0 https://github.com/OCA/web.git
 git clone -b 15.0 https://github.com/Intresco-SAS/website_axis_helpdesk.git
 git clone -b 15.0 https://github.com/Intresco-SAS/website_floating_whatsapp_icon.git
+git clone -b 15.0 https://github.com/Intresco-SAS/om_account_asset.git
 git clone https://github.com/Intresco-SAS/sh_pos_access_rights_hr.git
 git clone https://github.com/Intresco-SAS/pos_waiter_restrict_table.git
 git clone https://github.com/Intresco-SAS/pw_pos_waiter.git
