556c7cd8478d30a3458aac48ff1c266e429569be
DanielGR <danielgiral93@gmail.com>
2024-12-31 13:55:58 -0500

    add pos

--- script15.sh
+++ script15.sh
@@ -130,6 +130,7 @@ git clone -b 15.0 https://github.com/Intresco-SAS/sh_entmate_theme.git
 git clone -b 15.0 https://github.com/Intresco-SAS/base_account_budget.git
 git clone -b 15.0 https://github.com/Intresco-SAS/int_account_dynamic_reports.git
 git clone -b 15.0 https://github.com/Intresco-SAS/sh_ent_theme_config.git
+git clone -b 15.0 https://github.com/OCA/pos.git
 apt-get update
 timedatectl set-timezone America/Bogota
 service odoo restart
