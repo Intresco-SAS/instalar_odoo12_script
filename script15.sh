a8bf56f37cb6e34b3a808edac462e29ce08aa2ce
DanielGR <danielgiral93@gmail.com>
2025-02-06 11:36:27 -0500

    Update script15.sh

--- script15.sh
+++ script15.sh
@@ -145,7 +145,6 @@ git clone -b 15.0 https://github.com/Intresco-SAS/int_pos_proforma_receipt.git
 git clone https://github.com/Intresco-SAS/oi_account.git
 git clone -b 15.0 https://github.com/Intresco-SAS/oi_org_chart_account_group.git
 git clone -b 15.0 https://github.com/Intresco-SAS/oi_org_chart.git
-git clone -b 15.0 https://github.com/Intresco-SAS/bi_pos_reports.git
 apt-get update
 timedatectl set-timezone America/Bogota
 service odoo restart
